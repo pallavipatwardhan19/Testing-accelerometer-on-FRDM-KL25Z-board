b1b15e36a88b7bedc24fd58893287f388c11d9a7
Pallavi M Patwardhan <pallavipatwardhan19@gmail.com>
2017-11-23 03:31:29 -0600

    Update and rename Embed.c.rtf to Embed.c

---
+++ Embed.c
@@ -0,0 +1,19 @@
+/* -- Freescale FRDM board comes with an onboard accelerometer. For testing of accelerometer following code is used -- */ 
+#include "mbed.h"
+#include "MMA8451Q.h"
+#define ADDRESS (0x1d<<1)
+Serial pc(USBTX,USBRX);
+MMA8451Q acc(PTE25,PTE24,ADDRESS);
+float x,y,z;
+int main()
+{
+while(1)
+{
+x=acc.getAccX();		//For accelerometer values of x-axis
+y=acc.getAccY();		//For accelerometer values of y-axis
+z=acc.getAccZ();		//For accelerometer values of z-axis
+pc.printf("x=%f \t y=%f \t z=%f\t\n\r",x,y,z);
+wait(0.8);
+}
+Return 0;
+}
