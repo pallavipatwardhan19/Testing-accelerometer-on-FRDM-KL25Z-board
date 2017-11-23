7373e604cf46a761e3b36fb2856c742b102b5bae
Pallavi M Patwardhan <pallavipatwardhan19@gmail.com>
2017-11-23 03:34:04 -0600

    Update Embed.c

--- Embed.c
+++ Embed.c
@@ -7,13 +7,13 @@ MMA8451Q acc(PTE25,PTE24,ADDRESS);
 float x,y,z;
 int main()
 {
-while(1)
-{
-x=acc.getAccX();		//For accelerometer values of x-axis
-y=acc.getAccY();		//For accelerometer values of y-axis
-z=acc.getAccZ();		//For accelerometer values of z-axis
-pc.printf("x=%f \t y=%f \t z=%f\t\n\r",x,y,z);
-wait(0.8);
-}
-Return 0;
+    while(1)
+    {
+      x=acc.getAccX();		//For accelerometer values of x-axis
+      y=acc.getAccY();		//For accelerometer values of y-axis
+      z=acc.getAccZ();		//For accelerometer values of z-axis
+      pc.printf("x=%f \t y=%f \t z=%f\t\n\r",x,y,z);
+      wait(0.8);
+    }
+    return 0;
 }
